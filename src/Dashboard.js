83047c94ed6cc343bfab3460bb551f401464c906
Evan Wireman <evanwireman@evans-mbp.lan>
2020-11-07 15:19:26 -0500

    Makin dashboard nice

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import 'bootswatch/dist/lux/bootstrap.min.css';
-import './App.css';
+import './css/Dashboard.css';
 
 //Bootstrap components
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 class App extends Component {
@@ -15,8 +17,8 @@ class App extends Component {
   
       this.state = {
         username: "",
-        daysTilClean: 1,
-        daysClean: 0,
+        daysTilClean: 0,
+        daysClean: 7,
         partnerUsername: "",
       }
     }
@@ -29,9 +31,27 @@ class App extends Component {
     
     render(){
       return (
-        <Container fluid>
-            <Jumbotron>Sup yo</Jumbotron>
-        </Container>
+        <div className='OuterWrapper'>
+
+            <Nav className="navbar-dark bg-primary MainNav">
+                <h1 className="navbar-brand">Subtance Stopper</h1>
+            </Nav>
+
+            <Jumbotron>
+                <h1 className='text-center'>You have been { this.state.daysClean } days clean!</h1>
+            </Jumbotron>
+            <Nav fill variant="tabs" defaultActiveKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home">Find a group</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">Accountability Partner</Nav.Link>
+                </Nav.Item>
+                
+            </Nav>
+
+            
+        </div>
       ) 
     }
   }
