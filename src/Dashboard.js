6d749e215057bc607143fc19c97bdfd4ce9d26fe
Evan Wireman <evanwireman@evans-mbp.lan>
2020-11-07 16:30:28 -0500

    Finished rd of progress page

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,6 +10,9 @@ import Nav from 'react-bootstrap/Nav'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+//Custom components
+import Progress from './components/Progress';
+
 
 class App extends Component {
     constructor(props){
@@ -20,39 +23,61 @@ class App extends Component {
         daysTilClean: 0,
         daysClean: 7,
         partnerUsername: "",
+        key: "progress",
       }
+      this.handleSelect = this.handleSelect.bind(this);
     }
   
+    handleSelect(key){
+        this.setState({
+            key: key
+        })
+    }
   
     componentDidMount(){
-      
+      //fetch firebase info
     }
   
     
     render(){
-      return (
-        <div className='OuterWrapper'>
+        const key = this.state.key;
+        let content;
+        if (key === "progress"){
+            content = <Progress></Progress>
+        } else if (key === "group"){
+            content = <h1>Group</h1>
+        }else{
+            content = <h1>Partner</h1>
+        }
 
-            <Nav className="navbar-dark bg-primary MainNav">
-                <h1 className="navbar-brand">Subtance Stopper</h1>
-            </Nav>
 
-            <Jumbotron>
-                <h1 className='text-center'>You have been { this.state.daysClean } days clean!</h1>
-            </Jumbotron>
-            <Nav fill variant="tabs" defaultActiveKey="/home">
-                <Nav.Item>
-                    <Nav.Link href="/home">Find a group</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">Accountability Partner</Nav.Link>
-                </Nav.Item>
+        return (
+            <div className='OuterWrapper'>
+
+                <Nav className="navbar-dark bg-primary MainNav sticky-top">
+                    <h1 className="navbar-brand">Subtance Stopper</h1>
+                </Nav>
+
+                <Jumbotron>
+                    
+                    <h1 className='text-center'>You have been { this.state.daysClean } days clean!</h1>
+                </Jumbotron>
+                <Nav fill variant="tabs" defaultActiveKey="progress" onSelect={this.handleSelect}>
+                    <Nav.Item>
+                        <Nav.Link eventKey="progress">My Progress</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="group">Find a group</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="partner">Accountability Partner</Nav.Link>
+                    </Nav.Item>
+                </Nav>
                 
-            </Nav>
-
-            
-        </div>
-      ) 
+                {content}
+                
+            </div>
+        ) 
     }
   }
   
